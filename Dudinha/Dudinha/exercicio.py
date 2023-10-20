033ce87c6aea615147d0939f6beb92156571c269
duddagomess <126796489+duddagomess@users.noreply.github.com>
2023-10-20 11:07:17 -0300

    Add files via upload

---
+++ Dudinha/Dudinha/exercicio.py
@@ -0,0 +1,14 @@
+#Exercicio 1
+#print("O primeiro programa a gente nunca esquece")
+
+#Exercicio 2
+"""print("Maria Eduarda de Oliveira Gomes")
+print("Bauru")
+print("maria.eogomes3@gmail.com")"""
+
+#Exercicio 3
+"""print("XXXXX")
+print("X   X")
+print("X   X")
+print("X   X")
+print("XXXXX")"""
